909dbbfc5adf2804529a4b5e173807d6e26b1e9e
szymio11 <szymio11@gmail.com>
2018-08-11 17:01:14 +0200

    Seed Data

---
+++ Produkty.Repository/Migrations/20180809185841_SeedCategory.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Produkty.Repository.Migrations
+{
+    public partial class SeedCategory : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Categories (Id,Name) VALUES ('3e0231b3-a0aa-47a7-a845-3283a1732a12','Pieczywo')");
+            migrationBuilder.Sql("INSERT INTO Categories (Id,Name) VALUES ('3e0231b3-a0aa-47a7-a845-3283a1732a11','Chemia')");
+            migrationBuilder.Sql("INSERT INTO Categories (Id,Name) VALUES ('3e0231b3-a0aa-47a7-a845-3283a1732a13','Nabiał')");
+            migrationBuilder.Sql("INSERT INTO Categories (Id,Name) VALUES ('3e0231b3-a0aa-47a7-a845-3283a1722a13','Napoje')");
+            migrationBuilder.Sql("INSERT INTO Categories (Id,Name) VALUES ('3e0231b3-a0aa-47a7-a845-1283a1732a13','Mięso')");
+            migrationBuilder.Sql("INSERT INTO Categories (Id,Name) VALUES ('3e0231b3-a0aa-47a7-a845-3283a1712a16','Alkohole')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE * FROM Categories");
+        }
+    }
+}
---
+++ Produkty.Repository/Migrations/20180811143833_SeedProduct.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Produkty.Repository.Migrations
+{
+    public partial class SeedProduct : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Products (Id,CategoryId,Description,Name,Price) VALUES ('1e0231b3-a0aa-47a7-a845-3283a1732a12','3e0231b3-a0aa-47a7-a845-3283a1732a12','Do jedzenia!','Chleb','1.99')");
+            migrationBuilder.Sql("INSERT INTO Products (Id,CategoryId,Description,Name,Price) VALUES ('1e0231b3-a0aa-47a7-a845-3283a1732121','3e0231b3-a0aa-47a7-a845-3283a1732a13','Do jedzenia!','Jogurt Naturalny 200ml',1.59)");
+            migrationBuilder.Sql("INSERT INTO Products (Id,CategoryId,Description,Name,Price) VALUES ('1e0231b3-a0aa-47a7-a845-3283a1732321','3e0231b3-a0aa-47a7-a845-3283a1712a16','Do Picia, najlepiej smakuje schłodzony.','Piwo Tyskie 500ml but.',2.59)");
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Products WHERE Id='1e0231b3-a0aa-47a7-a845-3283a1732a12' OR id='1e0231b3-a0aa-47a7-a845-3283a1732121' OR id ='1e0231b3-a0aa-47a7-a845-3283a1732321'");
+        }
+    }
+}
